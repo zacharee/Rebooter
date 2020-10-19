4872346155fdd80dbe4731703a3411cff3da9860
zacharee <zachary.wander@gmail.com>
2020-10-19 09:38:28 -0400

    Allow showing over lock screen

--- app/src/main/java/tk/zwander/rebooter/MainActivity.kt
+++ app/src/main/java/tk/zwander/rebooter/MainActivity.kt
@@ -1,7 +1,9 @@
 package tk.zwander.rebooter
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.topjohnwu.superuser.Shell
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -18,6 +20,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            setShowWhenLocked(true)
+        } else {
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+            )
+        }
 
         buttons.adapter = ButtonAdapter()
     }
