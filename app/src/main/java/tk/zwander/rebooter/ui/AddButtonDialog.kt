bf14dd9ca0d5c91eeba3f32d6384a4b315817cd9
Zachary Wander <zachary.wander@gmail.com>
2020-10-22 21:24:00 -0400

    Fancy up add button dialog background

--- app/src/main/java/tk/zwander/rebooter/ui/AddButtonDialog.kt
+++ app/src/main/java/tk/zwander/rebooter/ui/AddButtonDialog.kt
@@ -1,11 +1,10 @@
 package tk.zwander.rebooter.ui
 
 import android.content.Context
-import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.add_button_dialog.*
-import kotlinx.android.synthetic.main.add_button_dialog.view.*
 import tk.zwander.rebooter.R
 import tk.zwander.rebooter.data.ButtonData
 
@@ -13,11 +12,19 @@ class AddButtonDialog(
     context: Context,
     private val items: List<ButtonData>,
     private val selectionCallback: (ButtonData) -> Unit
-) : MaterialAlertDialogBuilder(context) {
+) : MaterialAlertDialogBuilder(context, R.style.Theme_Rebooter_Dialog) {
     init {
         setTitle(R.string.add_button)
         setPositiveButton(android.R.string.cancel, null)
         setView(R.layout.add_button_dialog)
+        background = ContextCompat.getDrawable(context,
+            R.drawable.add_button_dialog_background)
+    }
+
+    override fun create(): AlertDialog {
+        return super.create().also {
+            it.window?.attributes?.windowAnimations = R.style.Theme_Rebooter_Dialog
+        }
     }
 
     override fun show(): AlertDialog {
@@ -28,7 +35,9 @@ class AddButtonDialog(
             }
             adapter.setItems(items)
 
-            dialog.buttons_list.adapter = adapter
+            dialog.buttons_list.postDelayed({
+                dialog.buttons_list.adapter = adapter
+            }, 100)
         }
     }
 }
